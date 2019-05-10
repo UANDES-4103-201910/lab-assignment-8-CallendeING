c1431a1a9ffc4a656143ee8721e107e691f9511f
Cristobal <callende@miuandes.cl>
2019-05-09 21:38:33 -0400

    writing

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -2,3 +2,15 @@
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
 });
+
+function hideShow(){
+	var keyboard = document.getElementById("kcontainer");
+	if(keyboard.style.display == "none"){
+		keyboard.style.display = "block";
+		$(btnHS).text("Hide");
+	}
+	else {
+		keyboard.style.display = "none";
+		$(btnHS).text("Show");
+	}
+}
